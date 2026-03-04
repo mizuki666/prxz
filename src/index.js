f34ed19cfbb48b85a1d9f61a0c36b087ba04221c
Воробьев Дмитрий Михайлович <Dmitriy.Vorobev@tularegion.ru>
2026-03-04 13:02:48 +0300

    add sld

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import { FormatValue } from './utils/formatValue.js';
 import { FormatDate } from './utils/formatDate.js';
 import { Logger } from './components/log/Logger.js';
+import { FilterReplaceText } from './utils/filterText.js';
+import { renderSlider } from './components/slider/Slider.js';
 
 import visi from './api/indexVisi.js';
 
-
 const prxz = {
     version: '1.0.1',
-    api:{
-        visi
+    api: {
+        visi,
     },
     lg: Logger,
     frm: {
@@ -17,7 +18,13 @@ const prxz = {
     },
     func: {
         FilterReplaceText,
-    }
+    },
+    comp: {
+        photo: {},
+        slider: {
+            render: renderSlider,
+        },
+    },
 };
 
 export default prxz;
